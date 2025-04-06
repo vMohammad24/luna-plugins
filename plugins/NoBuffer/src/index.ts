43faa062eacf89e98c77a23e1a7d811dc16c79ef
Inrixia <Inrixia@gmail.com>
2025-04-06 19:45:01 +1200

    NoBuffer - Only allow kicking once per itemId per session

--- plugins/NoBuffer/src/index.ts
+++ plugins/NoBuffer/src/index.ts
@@ -4,12 +4,15 @@ const trace = Tracer("[NoBuffer]");
 import { asyncDebounce } from "@inrixia/helpers";
 import { intercept } from "@neptune";
 
+import type { ItemId } from "neptune-types/tidal";
 import { Quality } from "../../../lib/classes/Quality";
 import { voidTrack } from "./voidTrack.native";
 
+const kickCache = new Set<ItemId>();
 const onStalled = asyncDebounce(async () => {
 	const mediaItem = await MediaItem.fromPlaybackContext();
-	if (mediaItem === undefined) return;
+	if (mediaItem === undefined || kickCache.has(mediaItem.id)) return;
+	kickCache.add(mediaItem.id);
 	trace.msg.log(`Playback stalled... Kicking tidal CDN!`);
 	await voidTrack(await mediaItem?.playbackInfo(Quality.Max.audioQuality)).catch(trace.err.withContext("voidTrack"));
 });
