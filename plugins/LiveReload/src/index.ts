1e9fc04f2f36eeeb402bbb0a01899a74e49e4fca
Inrixia <Inrixia@gmail.com>
2025-04-08 07:13:14 +1200

    LiveReload fixes

---
+++ plugins/LiveReload/src/index.ts
@@ -0,0 +1,22 @@
+import { Tracer } from "@inrixia/lib";
+const trace = Tracer("[LiveReload]");
+
+import { pluginStore, reloadPlugin } from "@neptune/plugins";
+import type { PluginManifest } from "neptune-types/api/plugins";
+
+const checkPlugins = async () => {
+	try {
+		for (const plugin of pluginStore) {
+			if (!plugin.enabled) continue;
+
+			const { hash, name }: PluginManifest = await fetch(`${new URL(plugin.id).href}/manifest.json`).then((res) => res.json());
+			if (hash !== plugin.manifest.hash) {
+				await reloadPlugin(plugin);
+				trace.msg.log(`Reloaded ${name}!`);
+			}
+		}
+	} catch {}
+};
+
+const interval = setInterval(checkPlugins, 1000);
+export const onUnload = () => clearInterval(interval);
