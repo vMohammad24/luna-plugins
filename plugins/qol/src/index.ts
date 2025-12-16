ff08994f7bd81ca4d42dfb61dc8fa60757ef0cc4
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2025-12-16 16:34:39 +0300

    feat(qol): add QoL plugin

---
+++ plugins/qol/src/index.ts
@@ -0,0 +1,17 @@
+import { LunaUnload, Tracer } from "@luna/core";
+import { redux } from "@luna/lib";
+import { settings } from "./settings";
+
+export const { trace } = Tracer("[QoL]");
+export const unloads = new Set<LunaUnload>();
+export { Settings } from "./settings";
+
+redux.intercept("player/SET_AVAILABLE_DEVICES", unloads, (payload: any) => {
+    if (settings.controllableVolume) {
+        redux.actions["player/SET_AVAILABLE_DEVICES"](payload.map((device: any) => ({
+            ...device,
+            controllableVolume: true
+        })));
+        return true;
+    }
+});
---
+++ plugins/qol/src/settings.tsx
@@ -0,0 +1,22 @@
+import { ReactiveStore } from "@luna/core";
+import { LunaSettings, LunaSwitchSetting } from "@luna/ui";
+import React from "react";
+export const settings = await ReactiveStore.getPluginStorage("qol", {
+    controllableVolume: true,
+});
+export const Settings = () => {
+    const [controllableVolume, setControllableVolume] = React.useState(settings.controllableVolume);
+    return (
+        <LunaSettings>
+            <LunaSwitchSetting
+                title="Controllable Volume"
+                desc="Enable or disable controllable volume for all your devices"
+                value={controllableVolume}
+                type="number"
+                onChange={(e) => {
+                    setControllableVolume(settings.controllableVolume = e.target.checked);
+                }}
+            />
+        </LunaSettings>
+    );
+};
